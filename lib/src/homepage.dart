cd15747190db5a7206e671b95b3da663f690ddad
oluwadamme <adeniyidamilola246@gmail.com>
2022-05-10 01:05:47 +0100

    add animation to UI

--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
+  double visible = 1;
   bool newPage = true;
   @override
   @override
@@ -24,7 +25,7 @@ class _HomePageState extends State<HomePage>
           top: 50,
           left: 20,
           child: Opacity(
-            opacity: newPage ? 1 : 0,
+            opacity: visible,
             child: Container(
               // duration: const Duration(seconds: 2),
               height: 60,
@@ -48,72 +49,71 @@ class _HomePageState extends State<HomePage>
           ),
         ),
         newPage
-            ? DraggableScrollableSheet(
-                initialChildSize: 0.5,
-                minChildSize: 0.5,
-                maxChildSize: 0.9,
-                builder: ((context, scrollController) {
-                  return Container(
-                    clipBehavior: Clip.hardEdge,
-                    // height: double.infinity,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20)),
-                    ), // decoration: const BoxDecoration(border: RoundedRectangleBorder()),
-                    child: ListView(
-                      controller: scrollController,
-                      shrinkWrap: true,
-                      // crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        //const SizedBox(height: 20),
-
-                        Center(
-                            child: Image.asset('assets/images/rectangle6.png')),
-                        const Text(
-                          'All Foods',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 24),
-                        ),
-                        const SizedBox(height: 10),
-                        GridView.builder(
-                            controller: scrollController,
-                            shrinkWrap: true,
-                            // physics: const NeverScrollableScrollPhysics(),
-                            itemCount: foods.length,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    crossAxisSpacing: 15,
-                                    mainAxisSpacing: 15),
-                            itemBuilder: (context, index) => GestureDetector(
-                                  onTap: () => setState(() {
-                                    newPage = !newPage;
-                                  }),
-                                  child: FoodCard(foods: foods[index]),
-                                )),
-                        Container(
-                          height: 90,
-                          width: 90,
-                          //transformAlignment: AlignmentGeometry.lerp(a, b, t),
-                          decoration: BoxDecoration(
-                            image: const DecorationImage(
-                                image:
-                                    AssetImage('assets/images/rectangle8.png')),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: const Icon(
-                            Icons.search,
-                            color: Colors.white,
-                            size: 30,
-                          ),
-                        )
-                      ],
-                    ),
-                  );
+            ? NotificationListener<DraggableScrollableNotification>(
+                onNotification: ((notification) {
+                  double visibility = 1 - notification.extent;
+                  if (visibility < 0.5) {
+                    setState(() {
+                      visible = visibility;
+                    });
+                  } else {
+                    setState(() {
+                      visible = 1;
+                    });
+                  }
+                  return true;
                 }),
+                child: DraggableScrollableSheet(
+                  initialChildSize: 0.5,
+                  minChildSize: 0.5,
+                  maxChildSize: 0.9,
+                  builder: ((context, scrollController) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20)),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 20),
+                          Center(
+                              child:
+                                  Image.asset('assets/images/rectangle6.png')),
+                          const SizedBox(height: 20),
+                          const Text(
+                            'All Foods',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24),
+                          ),
+                          Expanded(
+                            child: GridView.builder(
+                                controller: scrollController,
+                                shrinkWrap: true,
+                                // physics: const NeverScrollableScrollPhysics(),
+                                itemCount: foods.length,
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 2,
+                                        crossAxisSpacing: 15,
+                                        mainAxisSpacing: 15),
+                                itemBuilder: (context, index) =>
+                                    GestureDetector(
+                                      onTap: () => setState(() {
+                                        newPage = !newPage;
+                                        visible = 0;
+                                      }),
+                                      child: FoodCard(foods: foods[index]),
+                                    )),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+                ),
               )
             : DraggableScrollableSheet(
                 initialChildSize: 0.9,
@@ -178,6 +178,7 @@ class _HomePageState extends State<HomePage>
                           itemBuilder: (context, index) => GestureDetector(
                             onTap: () => setState(() {
                               newPage = !newPage;
+                              visible = 1;
                             }),
                             child: FoodTypeList(
                               foods: foods[index],
@@ -189,6 +190,32 @@ class _HomePageState extends State<HomePage>
                   );
                 }),
               ),
+        visible < 0.2 && newPage == true
+            ? GestureDetector(
+                onTap: () => setState(() {
+                  newPage = false;
+                  visible = 0;
+                }),
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    //transformAlignment: AlignmentGeometry.lerp(a, b, t),
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                          image: AssetImage('assets/images/rectangle8.png')),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              )
+            : Container()
       ],
     ));
   }
